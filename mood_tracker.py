1a0bd0eb046261b1fe0e52e70c352b02ee6c1ae7
Tiffany Pacek <tip46985@gmail.com>
2025-09-17 23:17:15 -0400

    Initial commit - Mood Tracker assignment

---
+++ mood_tracker.py
@@ -0,0 +1,22 @@
+Mood Report: One Mood Per Day
+
+On Monday, you were feeling energetic.
+On Tuesday, you were feeling sad.
+On Wednesday, you were feeling happy.
+On Thursday, you were feeling anxious.
+On Friday, you were feeling calm.
+On Saturday, you were feeling tired.
+On Sunday, you were feeling excited.
+
+==================================================
+
+Mood Tracker: Three Moods Per Day
+
+On Monday morning you were feeling calm.
+On Monday afternoon you were feeling sad.
+On Monday evening you were feeling excited.
+On Tuesday morning you were feeling tired.
+On Tuesday afternoon you were feeling happy.
+...
+
+
